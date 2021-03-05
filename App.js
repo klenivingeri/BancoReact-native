c196117cba9a88fc4c9b2d6aee9e2d05fbfda214
Erick <libertti@live.com>
2021-03-05 11:23:15 -0300

    Slider

--- App.js
+++ App.js
@@ -10,45 +10,32 @@ import {
   FlatList
   } from 'react-native';
 
-  import {Picker} from '@react-native-picker/picker';
+import Slider from '@react-native-community/slider';
 
 class App extends Component{
-
   constructor(props){
     super(props)
     this.state = {
-      pizza: 0,
-      pizzas: [
-        {key: '1', nome:'Strogonoff', valor: 60},
-        {key: '2', nome:'Calabresa', valor: 50},
-        {key: '3', nome:'Quatro queijos', valor: 60.99 },
-        {key: '4', nome:'Brigadeiro', valor: 30},
-        {key: '5', nome:'Banana', valor: 30}
-      ]
+      valor: 50
     }
   }
 
-
   render(){
 
-    let pizzaItem = this.state.pizzas.map((v,k) => {
-      return <Picker.item key={k} value={k} label={v.nome} />
-    })
     return(
       <View style={styles.container}> 
-
-        <Text style={styles.logo}> Menu Pizza</Text>
-
-        <Picker
-        selectedValue={this.state.pizza}
-        onValueChange={ (itemValue,itemIndex)=> this.setState({pizza: itemValue})}
-        >
-          {pizzaItem}
-        </Picker>
-
-        <Text  style={styles.pizza}>Você escolheu: {this.state.pizzas[this.state.pizza].nome}</Text>
-        <Text  style={styles.pizza}>R$:{this.state.pizzas[this.state.pizza].valor.toFixed(2)}</Text>
-   
+          <Slider
+          minimumValue={0}
+          maximumValue={100}
+          onValueChange={ (valorSelecionado) => this.setState({valor: valorSelecionado})}
+          value={this.state.valor}
+          minimumTrackTintColor="blue"
+          maximumTrackTintColor="red"
+          />
+          <Text style={{textAlign: 'center', fontSize: 20}}>
+            {this.state.valor.toFixed(0)}
+          </Text>
+        
       </View>    
     );
   }
@@ -58,22 +45,7 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     marginTop: 20,
-    
-
-  },
-  logo:{
-    textAlign: 'center',
-    fontSize:28,
-    fontWeight: 'bold'
-    
-  },
-  pizza:{
-    marginTop: 15,
-    fontSize:28,
-    textAlign: 'center'
   }
-
-
 });
 
 export default App;
@@ -90,7 +62,7 @@ export default App;
 o ScrollView vem com o  scrollEnabled false trava a tela, true solta a tela
 
 
-
+------------------------------------------------
 Pode criar uma pasta com o nome do component já representando o file, e salvar o  dentro dela  como index,
 não precisa colocar nome, pq o react já procura o index dentro da pasta
 
@@ -108,4 +80,8 @@ npm install @react-native-picker/picker​​
 
 import {Picker} from '@react-native-picker/picker';
 
+
+------------------------------------------------
+npm install @react-native-community/slider
+import Slider from '@react-native-community/slider'
 */
